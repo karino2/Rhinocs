9c649a861a91f5a9395526f64ccaea621fdbda83
Kazuma Arino <hogeika2@gmail.com>
2026-07-06 09:37:26 +0900

    bump version to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.github.karino2.rhinocs"
         minSdk = 29
         targetSdk = 37
-        versionCode = 1
-        versionName = "1"
+        versionCode = 2
+        versionName = "2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
